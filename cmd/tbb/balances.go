b561a012617283ded50b0014601e28e3a7da310f
Jessenth Ebenezer <30726289+sm1else-bot@users.noreply.github.com>
2021-12-09 20:36:08 +0530

    Update balances.go

--- cmd/tbb/balances.go
+++ cmd/tbb/balances.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/spf13/cobra"
 	"github.com/sm1else-bot/nekocoin-blockchain/database"
-
+)
 func balancesCmd() *cobra.Command {
 	var balancesCmd = &cobra.Command{
 		Use:   "balances",
