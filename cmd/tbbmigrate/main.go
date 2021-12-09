bd57334e59cd4d6780601688a6d78314f7dfaf21
Jessenth Ebenezer <30726289+sm1else-bot@users.noreply.github.com>
2021-12-09 20:28:25 +0530

    Add files via upload

---
+++ cmd/tbbmigrate/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/web3coach/the-blockchain-bar/database"
+)
+
+func main() {
+	state, err := database.NewStateFromDisk()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	defer state.Close()
+
+	block0 := database.NewBlock(
+		database.Hash{},
+		uint64(time.Now().Unix()),
+		[]database.Tx{
+			database.NewTx("Jess", "Jess", 3, ""),
+			database.NewTx("Jess", "Jess", 700, "reward"),
+		},
+	)
+
+	state.AddBlock(block0)
+	block0hash, _ := state.Persist()
+
+	block1 := database.NewBlock(
+		block0hash,
+		uint64(time.Now().Unix()),
+		[]database.Tx{
+			database.NewTx("Jess", "Harshith", 2000, ""),
+			database.NewTx("Jess", "Jess", 100, "reward"),
+			database.NewTx("Harshith", "Jess", 1, ""),
+			database.NewTx("Harshith", "Prasad", 1000, ""),
+			database.NewTx("Harshith", "Jess", 50, ""),
+			database.NewTx("Jess", "Jess", 600, "reward"),
+		},
+	)
+
+	state.AddBlock(block1)
+	state.Persist()
+}
---
+++ database/state.go
@@ -0,0 +1,147 @@
+package database
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+type State struct {
+	Balances  map[Account]uint
+	txMempool []Tx
+
+	dbFile          *os.File
+	latestBlockHash Hash
+}
+
+func NewStateFromDisk() (*State, error) {
+	cwd, err := os.Getwd()
+	if err != nil {
+		return nil, err
+	}
+
+	gen, err := loadGenesis(filepath.Join(cwd, "database", "genesis.json"))
+	if err != nil {
+		return nil, err
+	}
+
+	balances := make(map[Account]uint)
+	for account, balance := range gen.Balances {
+		balances[account] = balance
+	}
+
+	f, err := os.OpenFile(filepath.Join(cwd, "database", "block.db"), os.O_APPEND|os.O_RDWR, 0600)
+	if err != nil {
+		return nil, err
+	}
+
+	scanner := bufio.NewScanner(f)
+
+	state := &State{balances, make([]Tx, 0), f, Hash{}}
+
+	for scanner.Scan() {
+		if err := scanner.Err(); err != nil {
+			return nil, err
+		}
+
+		blockFsJson := scanner.Bytes()
+		var blockFs BlockFS
+		err = json.Unmarshal(blockFsJson, &blockFs)
+		if err != nil {
+			return nil, err
+		}
+
+		err = state.applyBlock(blockFs.Value)
+		if err != nil {
+			return nil, err
+		}
+
+		state.latestBlockHash = blockFs.Key
+	}
+
+	return state, nil
+}
+
+func (s *State) LatestBlockHash() Hash {
+	return s.latestBlockHash
+}
+
+func (s *State) AddBlock(b Block) error {
+	for _, tx := range b.TXs {
+		if err := s.AddTx(tx); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (s *State) AddTx(tx Tx) error {
+	if err := s.apply(tx); err != nil {
+		return err
+	}
+
+	s.txMempool = append(s.txMempool, tx)
+
+	return nil
+}
+
+func (s *State) Persist() (Hash, error) {
+	block := NewBlock(s.latestBlockHash, uint64(time.Now().Unix()), s.txMempool)
+	blockHash, err := block.Hash()
+	if err != nil {
+		return Hash{}, err
+	}
+
+	blockFs := BlockFS{blockHash, block}
+
+	blockFsJson, err := json.Marshal(blockFs)
+	if err != nil {
+		return Hash{}, err
+	}
+
+	fmt.Printf("Persisting new Block to disk:\n")
+	fmt.Printf("\t%s\n", blockFsJson)
+
+	if _, err = s.dbFile.Write(append(blockFsJson, '\n')); err != nil {
+		return Hash{}, err
+	}
+	s.latestBlockHash = blockHash
+
+	s.txMempool = []Tx{}
+
+	return s.latestBlockHash, nil
+}
+
+func (s *State) Close() error {
+	return s.dbFile.Close()
+}
+
+func (s *State) applyBlock(b Block) error {
+	for _, tx := range b.TXs {
+		if err := s.apply(tx); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (s *State) apply(tx Tx) error {
+	if tx.IsReward() {
+		s.Balances[tx.To] += tx.Value
+		return nil
+	}
+
+	if s.Balances[tx.From] < tx.Value {
+		return fmt.Errorf("insufficient balance")
+	}
+
+	s.Balances[tx.From] -= tx.Value
+	s.Balances[tx.To] += tx.Value
+
+	return nil
+}
